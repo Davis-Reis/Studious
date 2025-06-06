f98a54f31e4fae77b655dd2cce12d863fb1fe5ca
Christian Ken Castro <christiankenc@yahoo.com>
2025-06-05 20:55:32 -0600

    connected project to supabase:
    - tested with table

--- frontend/src/app/index.jsx
+++ frontend/src/app/index.jsx
@@ -1,4 +1,19 @@
 import { Text, View } from "react-native";
+import { supabase } from "../lib/supabase";
+
+async function testConnection() {
+  const { data, error } = await supabase.from("test").select("*").limit(1);
+
+  if (error) {
+    console.error("Supabase connection error:", error.message);
+    alert("Failed to connect to Supabase " + error.message);
+  } else {
+    console.log("Supabase connected successfully", data);
+    alert("Supabase connected successfully!");
+  }
+}
+
+testConnection();
 
 export default function Index() {
   return (
