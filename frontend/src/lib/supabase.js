4a5ec0f534b54c4602c3398e04b1bb9d2360906f
Christian Ken Castro <christiankenc@yahoo.com>
2025-06-05 22:19:15 -0600

    fixed .env, and .gitignore files:
    - removed keys from supabase.js and used .env to keep secrets hidden

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,7 +1,4 @@
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wdqgicsyxbdzfdougqkw.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkcWdpY3N5eGJkemZkb3VncWt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNzY3ODgsImV4cCI6MjA2NDY1Mjc4OH0.F6-w3QuQEQq5nWeugapZSQj4AM0u1_EU_Sk_X-HhMCA";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
